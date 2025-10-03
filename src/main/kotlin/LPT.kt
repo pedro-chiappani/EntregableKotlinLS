bd8efa270e3e75cd22d7242cfc569410e3e74986
pedro-chiappani <pepeelchapa@gmail.com>
2025-10-03 16:26:20 -0300

    agrego entorno de robocode

---
+++ src/main/kotlin/LPT.kt
@@ -0,0 +1,28 @@
+import robocode.JuniorRobot
+
+class LPT : JuniorRobot() {
+    var robotOnSight: Boolean = false
+
+    //    Estrategia estragia = new Agresivo(); Ejemplo de estrategia
+    override fun run() {
+        if (!robotOnSight) {
+            turnGunLeft(5)
+        } else {
+            robotOnSight = false
+        }
+        //        estrategia.run(); Como correrla
+    }
+
+
+    override fun onScannedRobot() {
+        robotOnSight = true
+        fire(2.0)
+        ahead(5)
+    }
+
+
+    override fun onHitWall() {
+        back(10)
+        turnLeft(45)
+    }
+}
